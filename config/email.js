5b3b62bb52389aff7787b06ea5a4960b6eb0b777
leobardo sanabria <34109009+leovoley2@users.noreply.github.com>
2020-06-02 03:17:03 -0500

    recien

--- config/email.js
+++ config/email.js
@@ -1,6 +1,39 @@
-module.exports = {
-    user: '012a6fd0365907',
-    pass: 'dbfd15ad446b7f',
-    host:'smtpout.secureserver.net',
-    port: '2525'
+const nodemailer = require('nodemailer');
+const emailConfig = require('../config/email');
+const fs = require('fs');
+const util = require('util');
+const ejs = require('ejs');
+
+let transport = nodemailer.createTransport({
+    host : emailConfig.host,
+    port : emailConfig.port, 
+    auth: {
+        user: emailConfig.user, 
+        pass: emailConfig.pass
+    }
+});
+
+exports.enviarEmail = async (opciones) => {
+    console.log(opciones);
+
+    // leer el archivo para el mail
+    const archivo = __dirname + `/../views/emails/${opciones.archivo}.ejs`;
+
+    // compilarlo
+    const compilado = ejs.compile(fs.readFileSync(archivo, 'utf8'));
+    
+    // crear el HTML
+    const html = compilado({ url : opciones.url });
+
+    // configurar las opciones del email
+    const opcionesEmail = {
+        from : 'Voleywebcamp <voleywebcamp@gmail.com>',
+        to : opciones.usuario.email,
+        subject: opciones.subject,
+        html
+    }
+
+    // enviar el mail
+    const sendEmail = util.promisify(transport.sendMail, transport);
+    return sendEmail.call(transport, opcionesEmail);
 }
