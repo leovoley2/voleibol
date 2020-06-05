3ac0874e551e329ab408d33eb4f0b3c722eb7e87
leobardo sanabria <34109009+leovoley2@users.noreply.github.com>
2020-06-05 15:38:03 -0500

    config

---
+++ config/cloudinaryconfig.js
@@ -0,0 +1,16 @@
+import { config, uploader } from 'cloudinary';
+
+import dotenv from 'dotenv';
+
+const cloudinaryConfig = (req, res, next) => {
+
+config  ({
+cloud_name: process.env.CLOUD_NAME,
+api_key: process.env.API_KEY,
+api_secret: process.env.API_SECRET,
+});
+
+next();
+
+}
+export { cloudinaryConfig, uploader };
--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
 const path = require('path');
 const bodyParser = require('body-parser');
+import { uploader, cloudinaryConfig } from './config/cloudinaryconfig'
 const flash = require('connect-flash');
 const session = require('express-session');
 const cookieParser = require('cookie-parser');
@@ -69,6 +70,8 @@ app.use((req, res, next) => {
     res.locals.year = fecha.getFullYear();
     next();
 });
+
+
 
 // Routing
 app.use('/', router());
